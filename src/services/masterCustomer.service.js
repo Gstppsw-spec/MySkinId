334cc6c1a3a38d7100437a48a03c5e10fdce7330
agusto <agusto.hawlai@gmail.com>
2026-03-11 11:27:18 +0700

    generate-otp

--- src/services/masterCustomer.service.js
+++ src/services/masterCustomer.service.js
@@ -309,8 +309,8 @@ class masterCustomerService {
       const { email, phoneNumber, password, loginMethod, countryCode } = data;
       console.log(data);
 
-      // const otp = masterCustomerService.generateOtp();
-      const otp = "123456";
+      const otp = masterCustomerService.generateOtp();
+      // const otp = "123456";
 
       if (loginMethod == "phone") {
         if (!password)
@@ -587,6 +587,8 @@ class masterCustomerService {
           pass: process.env.EMAIL_APP_PASSWORD,
         },
       });
+
+      console.log(transporter);
 
       const mailOptions = {
         from: `"MYSKIN.ID" <${process.env.EMAIL_USER}>`,
